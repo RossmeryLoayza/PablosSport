cc05a77f089cecb07d63596615033d6a1aafc06d
LuisAlexisSalazar <kirito_SWO@outlook.com>
2022-12-15 14:35:01 -0500

    Conexión con las Apis de puesto de venta incompleto

--- src/components/SalesPoint/CreateSalePoint.jsx
+++ src/components/SalesPoint/CreateSalePoint.jsx
@@ -1,61 +1,131 @@
-import React,{Component}  from "react";
+import React, {Component} from "react";
 import Layout from '../Layout/Layout'
 import Header from '../Layout/Header'
+//servicios
+import {ApiUrl, TokenFixed_of_UserRoot} from "../../services/apirest";
+//librerias
+import axios from "axios"
+import {Redirect} from "react-router-dom";
 
-class CreateSalePoint extends React.Component{
-    render(){
-        return(
-            <div>
-                <Header/>
-                <div className='row'>
-                    <Layout />
-                    <div className="col-sm-11 bg-light">
-                        <div className="bg-white">
-                        <div className="d-flex m-3 pt-3 justify-content-between">
-                                <div>
-                                    <h5>Crear Puesto de Venta</h5>
-                                </div>
-                                <div className="d-flex gap-2">
+class CreateSalePoint extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            isCreated: false
+        };
+        this.manejadorBoton = this.manejadorBoton.bind(this);
+    }
+
+    manejadorChange = async (e) => {
+        await this.setState({
+            name: e.target.value,
+        });
+    };
+
+    manejadorBoton = async () => {
+        const name = this.state.name
+        if (name.length !== 0) {
+            const UrlCreateSale = "api/pointsofsale/"
+            const res = await axios.post(ApiUrl + UrlCreateSale, {
+                "name": this.state.name
+            }, {
+                headers: {
+                    Authorization: 'Token ' + TokenFixed_of_UserRoot
+                }
+            }).then((response) => {
+                this.setState({isCreated: true});
+            }).catch(function (error) {
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+
+                    //Mostrar un mensaje al usuario :
+                    console.log("El nombre del puesto de venta ya existe")
+
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log('Error', error.message);
+                }
+            })
+        } else {
+            //Mostrar al usuario
+            console.log("Nombre vacio debes ingresar un nombre")
+
+        }
+    }
+
+    render() {
+        const {isCreated} = this.state
+        if (isCreated === true) {
+            console.log("Redirección a los puestos de venta");
+            return <Redirect to="/puesto_venta"/>
+        } else {
+            return (
+                <React.Fragment>
+                    <Header/>
+                    <div className='row'>
+                        <Layout/>
+                        <div className="col-sm-11 bg-light">
+                            <div className="bg-white">
+                                <div className="d-flex m-3 pt-3 justify-content-between">
                                     <div>
-                                        <a href="/puesto_venta">
-                                            <button type="button" className="btn" style={{backgroundColor:'#663399', color:'white'}}>Regresar</button>
-                                        </a>
+                                        <h5>Crear Puesto de Venta</h5>
                                     </div>
-                                </div>
-                            </div>
-                            <hr
-                                style={{background:'F8F9FA'}}
-                            />
-                            <div className="d-flex justify-content-center text-center p-5 m-5">
-                                <form>
-                                    <div className="form-group row gap-5">
+                                    <div className="d-flex gap-2">
                                         <div>
-                                            <label
-                                            style={{width:'180px',
-                                            clear:'left',
-                                            textAlign:'left',
-                                            paddingRight:'10px'}}> Nombre de Puesto de Venta: </label>
-                                            <input
-                                            style={{width:'250px'}}
-                                            name="company"
-                                            type="text"
-                                            placeholder="Ingrese nombre de empresa"
-                                            required
-                                            />
-                                        </div>
-                                        <div>
-                                            <a href="#modificar">
-                                                <button type="button" className="btn btn-primary">Guardar Puesto de Venta</button>
+                                            <a href="/puesto_venta">
+                                                <button type="button" className="btn"
+                                                        style={{backgroundColor: '#663399', color: 'white'}}>Regresar
+                                                </button>
                                             </a>
                                         </div>
                                     </div>
-                                </form>
+                                </div>
+                                <hr
+                                    style={{background: 'F8F9FA'}}
+                                />
+                                <div className="d-flex justify-content-center text-center p-5 m-5">
+                                    <form>
+                                        <div className="form-group row gap-5">
+                                            <div>
+                                                <label
+                                                    style={{
+                                                        width: '180px',
+                                                        clear: 'left',
+                                                        textAlign: 'left',
+                                                        paddingRight: '10px'
+                                                    }}> Nombre de Puesto de Venta: </label>
+                                                <input
+                                                    style={{width: '250px'}}
+                                                    name="company"
+                                                    type="text"
+                                                    placeholder="Ingrese nombre del puesto de venta"
+                                                    onChange={this.manejadorChange}
+
+                                                    required
+                                                />
+                                            </div>
+                                            <div>
+                                                <a href="#modificar">
+                                                    <button type="button" className="btn btn-primary"
+                                                            onClick={this.manejadorBoton}>Guardar Puesto de
+                                                        Venta
+                                                    </button>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        );
+                </React.Fragment>
+            )
+        }
     }
 }
+
 export default CreateSalePoint;
